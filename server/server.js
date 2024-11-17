0d4bb52471e415615c2f11da3da31df8389667eb
dgiakhanh <147835125+dgiakhanh@users.noreply.github.com>
2024-11-17 20:03:38 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const commonFeatureRouter = require("./routes/common/feature-routes");
 //create a separate file for this and then import/use that file here
 
 mongoose
-  .connect("db_url")
+  .connect("mongodb+srv://dgkhanh0912:dgkhanh0912@ttcs.wap7l.mongodb.net/")
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
